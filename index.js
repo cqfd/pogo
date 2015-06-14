2440307399e8525e49757604de3a02b36551e37a
Alan O'Donnell <alan.m.odonnell@gmail.com>
2015-06-14 18:33:50 -0400

    WIP much more symmetric now!

--- index.js
+++ index.js
@@ -19,36 +19,31 @@ function bounce(gen, input) {
   let output;
   try { output = gen.next(input); }
   catch (e) { return gen.reject(e); }
-  next(gen, output.value, output.done);
+  if (output.done) return gen.resolve(output.value);
+  next(gen, output.value);
 }
 
 function toss(gen, error) {
   let output;
   try { output = gen.throw(error); }
   catch (e) { return gen.reject(e); }
-  next(gen, output.value, output.done);
+  if (output.done) return gen.resolve(output.value);
+  next(gen, output.value);
 }
 
-function next(gen, instr, done) {
-  if (done) return gen.resolve(instr);
-  if (isPromise(instr)) return instr.then(i => bounce(gen, i), e => toss(gen, e));
-  if (instr instanceof Unbuffered) return instr.registerTaker({ gen: gen });
-  if (instr instanceof Take) return instr.ch.registerTaker({ gen: gen });
-  if (instr instanceof Put) {
-    const puter = { gen: gen };
-    return instr.ch.waitForTaker(puter, (t, tWasFirst) => {
-      if (tWasFirst) { resumeThem(); resumeUs(); } 
-      else { resumeUs(); resumeThem(); }
-      function resumeUs() { bounce(gen); }
-      function resumeThem() {
-        if (t.alt) {
-          invariant("Alternative take must be alive.", t.alt.alive);
-          t.alt.alive = false;
-        }
-        bounce(t.gen, instr.val);
-      }
-    });
+function next(gen, instr) {
+  if (isPromise(instr)) {
+    return instr.then(i => bounce(gen, i), e => toss(gen, e));
   }
+
+  if (instr instanceof Unbuffered) {
+    return instr.take(gen).then(i => bounce(gen, i), e => console.log("wtf", e));
+  }
+
+  if (instr instanceof Put) {
+    return instr.ch.put(gen, instr.val).then(() => bounce(gen), e => console.log("wtf", e));
+  }
+
   gen.reject(new Error("Invalid instruction: " + instr + "."));
 }
 
@@ -58,57 +53,42 @@ class Put {
     this.val = val;
   }
 }
-
-class Take {
-  constructor(ch) {
-    this.ch = ch;
-  }
-}
-
 const put = (ch, val) => new Put(ch, val);
-const take = ch => new Take(ch);
-const alts = ops => new Promise((resolve, reject) => {
-  let alive = true;
-  for (let op of ops) {
-    if (isPromise(op)) {
-      op.then(i => { if (alive) { alive = false; resolve(i); } },
-              e => { if (alive) { alive = false; reject(e); } });
-    } else if (op instanceof Unbuffered) {
-    } else if (op instanceof Take) {
-    } else if (op instanceof Put) {
-    }
-  }
-});
 
 const isPromise = x => typeof x.then === 'function';
-const isAlt = x => x.alts != undefined;
-const isAlive = x => !isAlt(x) || x.alts.alive;
 const isGen = x => typeof x.next === 'function' && typeof x.throw === 'function';
 
 class Unbuffered {
+
   constructor() {
+    this.takings = [];
     this.putings = [];
-    this.takers = [];
   }
 
-  waitForTaker(puter, cb, eb) {
-    this.takers = this.takers.filter(isAlive);
-
-    if (!isAlive(puter)) return eb();
-    if (!this.takers.length) return this.putings.push(cb);
-
-    const taker = this.takers[0];
-    const takerWasFirst = true;
-
-    if (cb(taker, takerWasFirst)) this.takers.shift();
+  put(puter, val) {
+    const takings = this.takings;
+    const putings = this.putings;
+    return new Promise((resolve, reject) => {
+      if (!takings.length) {
+        return putings.push({ val: val, resolve: resolve, reject: reject });
+      }
+      const taking = takings.shift();
+      taking.resolve(val);
+      resolve();
+    });
   }
 
-  registerTaker(taker) {
-    if (!isAlive(taker)) return;
-    if (!this.putings.length) return this.takers.push(taker);
-    const resolve = this.putings.shift();
-    taker.wasFirst = false;
-    resolve(taker);
+  take(taker) {
+    const takings = this.takings;
+    const putings = this.putings;
+    return new Promise((resolve, reject) => {
+      if (!putings.length) {
+        return takings.push({ resolve: resolve, reject: reject });
+      }
+      const puting = putings.shift();
+      puting.resolve();
+      resolve(puting.val);
+    });
   }
 }
 
@@ -118,8 +98,6 @@ const chan = () => new Unbuffered();
 module.exports = {
   go: go,
   put: put,
-  take: take,
-  alts: alts,
   sleep: sleep,
   chan: chan
 };
