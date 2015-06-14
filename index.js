8863e10d0f0d8f1e28307547051b2bda3841e10c
Alan O'Donnell <alan.m.odonnell@gmail.com>
2015-06-14 12:48:16 -0400

    WIP. About to get alts working agian.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-/*
- * The trampoline. (Lots of inspiration taken from github.com/tj/co.)
- */
+const assert = require('assert');
+const invariant = (msg, assertion) => {
+  console.log("Invariant:", msg);
+  assert(assertion, msg);
+}
 
 function go(star, args) {
   return new Promise((resolve, reject) => {
@@ -15,125 +17,113 @@ function go(star, args) {
 
 function bounce(gen, input) {
   let output;
-  try { output = gen.next(input) }
+  try { output = gen.next(input); }
   catch (e) { return gen.reject(e); }
-  next(gen, output);
+  next(gen, output.value, output.done);
 }
 
 function toss(gen, error) {
   let output;
   try { output = gen.throw(error); }
   catch (e) { return gen.reject(e); }
-  next(gen, output);
+  next(gen, output.value, output.done);
 }
 
-function next(gen, output) {
-  const v = output.value;
-  if ( output.done ) gen.resolve(v);
-  else if ( isPromise(v) ) v.then(i => bounce(gen, i), e => toss(gen, e));
-  else if ( isPut(v) ) v[0].put(gen, v[1]);
-  else if ( isTake(v) ) v.take(gen);
-  else gen.reject("Invalid yield in go block.");
+function next(gen, instr, done) {
+  if (done) return gen.resolve(instr);
+  if (isPromise(instr)) return instr.then(i => bounce(gen, i), e => toss(gen, e));
+  if (instr instanceof Unbuffered) return instr.registerTaker({ gen: gen });
+  if (instr instanceof Take) return instr.ch.registerTaker({ gen: gen });
+  if (instr instanceof Put) {
+    const puter = { gen: gen };
+    return instr.ch.waitForTaker(puter).then(t => {
+      if (t.wasFirst) { resumeThem(); resumeUs(); } 
+      else { resumeUs(); resumeThem(); }
+
+      function resumeUs() { bounce(gen); }
+      function resumeThem() {
+        if (t.alt) {
+          invariant("Alternative take must be alive.", t.alt.alive);
+          t.alt.alive = false;
+        }
+        bounce(t.gen, instr.val);
+      }
+    });
+  }
+  gen.reject(new Error("Invalid instruction: " + instr + "."));
 }
 
-/*
- * Go block operations.
- */
+class Put {
+  constructor(ch, val) {
+    this.ch = ch;
+    this.val = val;
+  }
+}
 
-const put = (ch, val) => [ch, val];
-const take = ch => ch;
-const alts = ops => new Promise((resolve, reject) => {
-  const alt = { alive: true, resolve: resolve };
-  ops.forEach(op => {
-    if ( isPromise(op) ) {
-      op.then(v => { if ( alt.alive ) { alt.alive = false; resolve(v); } }, 
-              e => { if ( alt.alive ) { alt.alive = false; reject(e); } });
-    } else if ( isPut(op) ) { 
-      op[0].put(alt, op[1]);
-    } else if ( isTake(op) ) {
-      op.take(alt);
-    } else {
-      throw "Unsupported alt operation."
-    }
-  });
-});
+class Take {
+  constructor(ch) {
+    this.ch = ch;
+  }
+}
+
+class Alts {
+  constructor(ops) {
+    this.ops = ops;
+    this.alive = true;
+  }
+}
+
+const put = (ch, val) => new Put(ch, val);
+const take = ch => new Take(ch);
+const alts = ops => new Alts(ops);
 
 const isPromise = x => typeof x.then === 'function';
-const util = require('util');
-const isChan = x => typeof x.put === 'function' && typeof x.take === 'function';
-const isPut = x => util.isArray(x) && x.length == 2 && isChan(x[0]);
-const isTake = isChan; 
-const isAlt = x => x.alive != undefined && typeof x.resolve === 'function';
+const isAlt = x => x.alts != undefined;
+const isAlive = x => !isAlt(x) || x.alts.alive;
 const isGen = x => typeof x.next === 'function' && typeof x.throw === 'function';
 
-/*
- * Simple unbuffered channels.
- */
-
 class Unbuffered {
+  /*
+   * taker: { gen, alts }
+   * puter: { gen, alts }
+   * puting: promise resolver
+   */
+
   constructor() {
     this.putings = [];
     this.takers = [];
   }
 
-  put(puter, val) {
-    if ( isAlt(puter) && !puter.alive ) return;
-
-    this.takers = this.takers.filter(t => isGen(t) || t.alive);
-    if ( !this.takers.length ) return this.putings.push({ puter: puter, val: val });
-
-    const taker = this.takers.shift();
-
-    if ( isAlt(taker) && isAlt(puter) ) {
-      taker.alive = puter.alive = false;
-      taker.resolve({ channel: this, value: val });
-      puter.resolve({ channel: this });
-    } else if ( isAlt(taker) && isGen(puter) ) {
-      taker.alive = false;
-      taker.resolve({ channel: this, value: val });
-      bounce(puter);
-    } else if ( isGen(taker) && isAlt(puter) ) {
-      puter.alive = false;
-      bounce(taker, val);
-      puter.resolve({ channel: this });
-    } else {
-      bounce(taker, val);
-      bounce(puter);
-    }
+  waitForTaker(puter) {
+    this.takers = this.takers.filter(isAlive);
+    const takers = this.takers;
+    const putings = this.putings;
+    return new Promise((resolve, reject) => {
+      if (!isAlive(puter)) return reject();
+      if (!takers.length) return putings.push(resolve);
+      const taker = takers.shift();
+      taker.wasFirst = true;
+      resolve(taker);
+    });
   }
 
-  take(taker) {
-    if ( isAlt(taker) && !taker.alive ) return;
-
-    this.putings = this.putings.filter(p => isGen(p.puter) || p.puter.alive);
-    if ( !this.putings.length ) return this.takers.push(taker);
-
-    const puting = this.putings.shift(),
-          puter = puting.puter;
-
-    if ( isAlt(puter) && isAlt(taker) ) {
-      puter.alive = taker.alive = false;
-      puter.resolve({ channel: this });
-      taker.resolve({ channel: this, value: puting.val });
-    } else if ( isAlt(puter) && isGen(taker) ) {
-      puter.alive = false;
-      puter.resolve({ channel: this });
-      bounce(taker, puting.val);
-    } else if ( isGen(puter) && isAlt(taker) ) {
-      taker.alive = false;
-      bounce(puter);
-      taker.resolve({ channel: this, value: puting.val });
-    } else {
-      bounce(puter);
-      bounce(taker, puting.val);
-    }
+  registerTaker(taker) {
+    if (!isAlive(taker)) return;
+    if (!this.putings.length) return this.takers.push(taker);
+    const resolve = this.putings.shift();
+    taker.wasFirst = false;
+    resolve(taker);
   }
 }
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const chan = () => new Unbuffered();
 
 module.exports = {
   go: go,
   put: put,
   take: take,
   alts: alts,
-  Unbuffered: Unbuffered
+  sleep: sleep,
+  chan: chan
 };
