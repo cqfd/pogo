5ce94ebe3bc690ba94465cdcd0c8929ae7f43dfd
Alan O'Donnell <alan.m.odonnell@gmail.com>
2015-06-14 20:20:45 -0400

    Alternate!

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const invariant = (msg, assertion) => {
-  console.log("Invariant:", msg);
-  assert(assertion, msg);
-}
+const wtf = e => console.log("wtf", e);
 
 function go(star, args) {
   return new Promise((resolve, reject) => {
@@ -37,14 +34,48 @@ function next(gen, instr) {
   }
 
   if (instr instanceof Unbuffered) {
-    return instr.take().then(i => bounce(gen, i), e => console.log("wtf", e));
+    return instr.take(gen).then(i => bounce(gen, i), wtf);
   }
 
   if (instr instanceof Put) {
-    return instr.ch.put(instr.val).then(() => bounce(gen), e => console.log("wtf", e));
+    return instr.ch.put(gen, instr.val).then(() => bounce(gen), wtf);
   }
 
-  gen.reject(new Error("Invalid instruction: " + instr + "."));
+  if (instr instanceof Alts) {
+    const alt = { isLive: true };
+    for (let op of instr.ops) {
+      if (isPromise(op)) {
+        op.then(i => {
+          if (alt.isLive) {
+            alt.isLive = false;
+            bounce(gen, i);
+          }
+        }, e => {
+          if (alt.isLive) {
+            alt.isLive = false;
+            toss(gen, e);
+          }
+        });
+      }
+
+      if (op instanceof Unbuffered) {
+        op.take(alt).then(i => {
+          alt.isLive = false;
+          bounce(gen, { value: i, channel: op })
+        }, wtf);
+      }
+
+      if (op instanceof Put) {
+        op.ch.put(alt, instr.val).then(() => {
+          alt.isLive = false;
+          bounce(gen, { channel: op });
+        }, wtf);
+      }
+    }
+    return;
+  }
+
+  gen.reject(new Error("Invalid yield instruction: " + instr + "."));
 }
 
 class Put {
@@ -55,8 +86,20 @@ class Put {
 }
 const put = (ch, val) => new Put(ch, val);
 
+class Alts {
+  constructor(ops) {
+    this.ops = ops;
+  }
+}
+const alts = ops => new Alts(ops);
+
 const isPromise = x => typeof x.then === 'function';
 const isGen = x => typeof x.next === 'function' && typeof x.throw === 'function';
+const isLive = x => {
+  const alive = x.isLive || x.isLive === undefined;
+  if (!alive) console.log("Found something dead!", x);
+  return alive;
+}
 
 class Unbuffered {
 
@@ -65,12 +108,18 @@ class Unbuffered {
     this.putings = [];
   }
 
-  put(val) {
+  put(puter, val) {
+    this.takings = this.takings.filter(isLive);
     const takings = this.takings;
     const putings = this.putings;
     return new Promise((resolve, reject) => {
       if (!takings.length) {
-        return putings.push({ val: val, resolve: resolve, reject: reject });
+        return putings.push({
+          val: val,
+          resolve: resolve,
+          reject: reject,
+          isLive: puter.isLive
+        });
       }
       const taking = takings.shift();
       taking.resolve(val);
@@ -78,12 +127,17 @@ class Unbuffered {
     });
   }
 
-  take() {
+  take(taker) {
+    this.putings = this.putings.filter(isLive);
     const takings = this.takings;
     const putings = this.putings;
     return new Promise((resolve, reject) => {
       if (!putings.length) {
-        return takings.push({ resolve: resolve, reject: reject });
+        return takings.push({
+          resolve: resolve,
+          reject: reject,
+          isLive: taker.isLive
+        });
       }
       const puting = putings.shift();
       puting.resolve();
@@ -92,12 +146,13 @@ class Unbuffered {
   }
 }
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 const chan = () => new Unbuffered();
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 module.exports = {
   go: go,
   put: put,
+  alts: alts,
   sleep: sleep,
   chan: chan
 };
