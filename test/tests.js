b973609c06b9b73763e2201c0f09498d770e4406
Alan O'Donnell <alan.m.odonnell@gmail.com>
2015-06-25 18:00:31 -0400

    Style.

--- test/tests.js
+++ test/tests.js
@@ -1,234 +1,238 @@
 'use strict';
 
-const assert = require("chai").assert;
-const pogo = require('../index.js'),
-      race = pogo.race,
-      chan = pogo.chan,
-      put = pogo.put;
-const wtf = x => console.log("wtf", x);
-const sleep = ms => new Promise(awaken => setTimeout(awaken, ms));
+import { assert } from 'chai'
+import pogo, { chan, put, race } from '../index.js'
 
-describe("returning a value", () => {
+const wtf = x => console.log('wtf', x)
+const sleep = ms => new Promise(awaken => setTimeout(awaken, ms))
+
+describe('returning a value', () => {
   it("resolves the pogo's promise", () => {
-    return pogo(function*() { return 123; }).then(v => assert.equal(v, 123)); });
-});
+    return pogo(function*() {
+      return 123
+    }).then(v => assert.equal(v, 123)) })
+})
 
-describe("raising an exception", () => {
+describe('raising an exception', () => {
   it("rejects the pogo's promise", () => {
-    return pogo(function*() { throw "bang!"; }).catch(e => assert.equal(e, "bang!"));
-  });
-});
+    return pogo(function*() {
+      throw 'bang!'
+    }).catch(e => assert.equal(e, 'bang!'))
+  })
+})
 
-describe("yielding a promise", () => {
-  describe("that resolves", () => {
-    it("resumes the pogo with the resolution", () => {
-      return pogo(function*() { assert.equal(yield Promise.resolve(123), 123)});
-    });
-  });
-
-  describe("that rejects", () => {
-    it("throws the rejection back into the pogo", () => {
+describe('yielding a promise', () => {
+  describe('that resolves', () => {
+    it('resumes the pogo with the resolution', () => {
       return pogo(function*() {
-        try { yield Promise.reject("bang!"); }
-        catch (e) { assert.equal(e, "bang!") }
-      });
-    });
-  });
-});
+        assert.equal(yield Promise.resolve(123), 123)
+      })
+    })
+  })
 
-describe("yielding a put or a take", () => {
-  it("resumes the pogo with a corresponding take or put", () => {
-    const ch = chan();
+  describe('that rejects', () => {
+    it('throws the rejection back into the pogo', () => {
+      return pogo(function*() {
+        try { yield Promise.reject('bang!') }
+        catch (e) { assert.equal(e, 'bang!') }
+      })
+    })
+  })
+})
+
+describe('yielding a put or a take', () => {
+  it('resumes the pogo with a corresponding take or put', () => {
+    const ch = chan()
     pogo(function*() {
       for (let i = 0; i < 10; i++) {
         yield put(ch, i)
       }
-    });
+    })
     return pogo(function*() {
       for (let i = 0; i < 10; i++) {
-        assert.equal(i, yield ch);
+        assert.equal(i, yield ch)
       }
-    });
-  });
-});
+    })
+  })
+})
 
-describe("race", () => {
-  it("works with promises", () => {
+describe('race', () => {
+  it('works with promises', () => {
     return pogo(function*() {
-      const ret = yield race([sleep(1000), Promise.resolve("woot")]);
-      assert.equal(ret, "woot");
-    });
-  });
+      const ret = yield race([sleep(1000), Promise.resolve('woot')])
+      assert.equal(ret, 'woot')
+    })
+  })
 
-  it("cancels slow alternatives", () => {
-    const log = [];
-    const ch1 = chan();
-    const ch2 = chan();
+  it('cancels slow alternatives', () => {
+    const log = []
+    const ch1 = chan()
+    const ch2 = chan()
 
     pogo(function*() {
-      yield race([put(ch1, "first"), put(ch2, "second")]);
-      log.push("put first");
-      yield put(ch2, "deuxieme");
-      log.push("put deuxieme");
-    }).catch(wtf);
+      yield race([put(ch1, 'first'), put(ch2, 'second')])
+      log.push('put first')
+      yield put(ch2, 'deuxieme')
+      log.push('put deuxieme')
+    }).catch(wtf)
 
     return pogo(function*() {
-      const r1 = yield race([ch1, ch2]);
-      log.push("alted");
-      assert.equal(r1.channel, ch1);
-      assert.equal(r1.value, "first");
+      const r1 = yield race([ch1, ch2])
+      log.push('alted')
+      assert.equal(r1.channel, ch1)
+      assert.equal(r1.value, 'first')
 
-      const r2 = yield race([ch1, ch2]);
-      log.push("alted");
-      assert.equal(r2.channel, ch2);
-      assert.equal(r2.value, "deuxieme");
+      const r2 = yield race([ch1, ch2])
+      log.push('alted')
+      assert.equal(r2.channel, ch2)
+      assert.equal(r2.value, 'deuxieme')
 
-      assert.deepEqual(log, ["put first", "alted", "put deuxieme", "alted"]);
-    });
-  });
-});
+      assert.deepEqual(log, ['put first', 'alted', 'put deuxieme', 'alted'])
+    })
+  })
+})
 
-describe("yielding a generator function", () => {
-  it("works", () => {
+describe('yielding a generator function', () => {
+  it('works', () => {
     function* example(ch) {
-      const x = yield Promise.resolve(123);
+      const x = yield Promise.resolve(123)
 
-      let y;
-      try { yield Promise.reject("bang!"); }
-      catch (e) { y = 456; }
+      let y
+      try { yield Promise.reject('bang!') }
+      catch (e) { y = 456 }
 
-      const z = yield ch;
+      const z = yield ch
 
-      return [x, y, z];
+      return [x, y, z]
     }
 
     return pogo(function*() {
-      const ch = chan();
+      const ch = chan()
       pogo(function*() {
-        yield put(ch, "sup");
-      });
+        yield put(ch, 'sup')
+      })
 
-      assert.deepEqual(yield example(ch), [123, 456, "sup"]);
-    });
-  });
+      assert.deepEqual(yield example(ch), [123, 456, 'sup'])
+    })
+  })
 
-  it("is safe even if you yield the same generator multiple times", () => {
+  it('is safe even if you yield the same generator multiple times', () => {
     function* star(x) {
-      const y = yield Promise.resolve("foo");
-      const z = yield Promise.resolve("bar");
-      return [x, y, z];
+      const y = yield Promise.resolve('foo')
+      const z = yield Promise.resolve('bar')
+      return [x, y, z]
     }
     return pogo(function*() {
-      const gen = star(123);
-      assert.equal(123, yield race([gen, Promise.resolve(123)]));
-      assert.equal(456, yield race([gen, Promise.resolve(456)]));
-      assert.deepEqual(yield race([gen, Promise.resolve(789)]), [123, "foo", "bar"]);
-    });
-  });
-});
+      const gen = star(123)
+      assert.equal(123, yield race([gen, Promise.resolve(123)]))
+      assert.equal(456, yield race([gen, Promise.resolve(456)]))
+      assert.deepEqual(yield race([gen, Promise.resolve(789)]), [123, 'foo', 'bar'])
+    })
+  })
+})
 
-describe("async/callback-based interface", () => {
-  describe("putAsync", () => {
-    it("works", (done) => {
-      const ch = chan();
+describe('async/callback-based interface', () => {
+  describe('putAsync', () => {
+    it('works', (done) => {
+      const ch = chan()
       pogo(function*() {
-        assert.equal(yield ch, 123);
-      });
-      ch.putAsync(123, done);
-    });
-  });
+        assert.equal(yield ch, 123)
+      })
+      ch.putAsync(123, done)
+    })
+  })
 
-  describe("takeAsync", () => {
-    it("works", (done) => {
-      const ch = chan();
+  describe('takeAsync', () => {
+    it('works', (done) => {
+      const ch = chan()
       pogo(function*() {
-        yield put(ch, 123);
-      });
+        yield put(ch, 123)
+      })
       ch.takeAsync(v => {
-        assert.equal(v, 123);
-        return done();
-      });
-    });
-  });
-});
+        assert.equal(v, 123)
+        return done()
+      })
+    })
+  })
+})
 
-describe("sharing a channel between multiple pogos", () => {
-  it("works", () => {
-    const log = [];
-    const ch = chan();
+describe('sharing a channel between multiple pogos', () => {
+  it('works', () => {
+    const log = []
+    const ch = chan()
     pogo(function*() {
-      yield put(ch, "foo");
-      log.push("put foo");
-    }).catch(wtf);
+      yield put(ch, 'foo')
+      log.push('put foo')
+    }).catch(wtf)
     pogo(function*() {
-      yield put(ch, "bar");
-      log.push("put bar");
-    }).catch(wtf);
+      yield put(ch, 'bar')
+      log.push('put bar')
+    }).catch(wtf)
     pogo(function*() {
-      yield put(ch, "baz");
-      log.push("put baz");
-    }).catch(wtf);
+      yield put(ch, 'baz')
+      log.push('put baz')
+    }).catch(wtf)
     return pogo(function*() {
-      assert.equal(yield ch, "foo");
-      log.push("took foo");
-      assert.equal(yield ch, "bar");
-      log.push("took bar");
-      assert.equal(yield ch, "baz");
-      log.push("took baz");
+      assert.equal(yield ch, 'foo')
+      log.push('took foo')
+      assert.equal(yield ch, 'bar')
+      log.push('took bar')
+      assert.equal(yield ch, 'baz')
+      log.push('took baz')
       assert.deepEqual(log, [
-        "put foo", "took foo", "put bar", "took bar", "put baz", "took baz"
-      ]);
-    });
-  });
-});
+        'put foo', 'took foo', 'put bar', 'took bar', 'put baz', 'took baz'
+      ])
+    })
+  })
+})
 
-describe("puting and taking from the same channel", () => {
-  it("works", () => {
-    const log = [];
-    const ch = chan();
+describe('puting and taking from the same channel', () => {
+  it('works', () => {
+    const log = []
+    const ch = chan()
 
     pogo(function*() {
-      const ping = yield ch;
-      log.push("received " + ping);
-      yield pogo.put(ch, "pong");
-      log.push("sent pong");
-    }).catch(wtf);
+      const ping = yield ch
+      log.push('received ' + ping)
+      yield pogo.put(ch, 'pong')
+      log.push('sent pong')
+    }).catch(wtf)
 
     return pogo(function*() {
-      yield pogo.put(ch, "ping");
-      log.push("sent ping");
-      const pong = yield ch;
-      log.push("received " + pong);
+      yield pogo.put(ch, 'ping')
+      log.push('sent ping')
+      const pong = yield ch
+      log.push('received ' + pong)
 
-      assert.deepEqual(log, ["received ping", "sent ping", "sent pong", "received pong"]);
-    });
-  });
-});
+      assert.deepEqual(log, ['received ping', 'sent ping', 'sent pong', 'received pong'])
+    })
+  })
+})
 
-describe("fairness", () => {
-  it("works for a take and then a put", () => {
-    const log = [];
-    const ch = chan();
+describe('fairness', () => {
+  it('works for a take and then a put', () => {
+    const log = []
+    const ch = chan()
     pogo(function*() {
-      yield ch;
-      log.push("took");
-    });
+      yield ch
+      log.push('took')
+    })
     return pogo(function*() {
-      yield put(ch);
-      log.push("put");
-    }).then(() => assert.deepEqual(log, ["took", "put"]));
-  });
+      yield put(ch)
+      log.push('put')
+    }).then(() => assert.deepEqual(log, ['took', 'put']))
+  })
 
-  it("works for a put and then a take", () => {
-    const log = [];
-    const ch = chan();
+  it('works for a put and then a take', () => {
+    const log = []
+    const ch = chan()
     pogo(function*() {
-      yield put(ch);
-      log.push("put");
-    });
+      yield put(ch)
+      log.push('put')
+    })
     return pogo(function*() {
-      yield ch;
-      log.push("took");
-    }).then(() => assert.deepEqual(log, ["put", "took"]));
-  });
-});
+      yield ch
+      log.push('took')
+    }).then(() => assert.deepEqual(log, ['put', 'took']))
+  })
+})
